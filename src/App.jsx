ae8c76ffe0e439bda7c036e652799d8cbf02cce1
Daniel Marcovich <danielmark999@gmail.com>
2022-09-30 11:03:39 -0500

    feat(core): Adding eslint config - prettier config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
-
 const App = () => {
-  return (
-    <div>
-      <h1>
-        REACT TEMPLATE
-      </h1>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1>REACT TEMPLATE</h1>
+		</div>
+	);
+};
 
-export default App
+export default App;
